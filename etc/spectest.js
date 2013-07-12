7e29b0442433b03d0290415c8167cbfee491c324
Ben Beltran <ben@freshout.us>
2013-07-12 14:44:56 -0500

    Properly export the parser.

--- etc/spectest.js
+++ etc/spectest.js
@@ -1,4 +1,9 @@
+fs = require("fs");
 Thulium = require('thulium');
+
+var template = fs.readFileSync('etc/template.tm', {encoding : 'utf8'});
+
+tm = new Thulium({template : template});
 
 repl = require('repl');
 repl.start({
